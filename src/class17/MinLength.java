1b9ce104bcbc0610e1e46c1480282789b9d5c019
runqi-zhao <529978097@qq.com>
2022-07-20 14:53:33 +0800

    Morris

---
+++ src/class17/MinLength.java
@@ -0,0 +1,116 @@
+package class17;
+
+public class MinLength {
+
+    public static class Node {
+        public int val;
+        public Node left;
+        public Node right;
+
+        public Node(int x) {
+            val = x;
+        }
+    }
+
+    public static int minHeight1(Node head) {
+        if (head == null) {
+            return 0;
+        }
+        return process(head);
+    }
+
+    private static int process(Node head) {
+        if (head.left == null && head.right == null) {
+            return 1;
+        }
+        int leftHeight = Integer.MAX_VALUE;
+        if (head.left != null) {
+            leftHeight = process(head.left);
+        }
+        int rightHeight = Integer.MAX_VALUE;
+        if (head.right != null) {
+            rightHeight = process(head.right);
+        }
+        return 1 + Math.min(leftHeight, rightHeight);
+    }
+
+
+    // for test
+    public static Node generateRandomBST(int maxLevel, int maxValue) {
+        return generate(1, maxLevel, maxValue);
+    }
+
+    // for test
+    public static Node generate(int level, int maxLevel, int maxValue) {
+        if (level > maxLevel || Math.random() < 0.5) {
+            return null;
+        }
+        Node head = new Node((int) (Math.random() * maxValue));
+        head.left = generate(level + 1, maxLevel, maxValue);
+        head.right = generate(level + 1, maxLevel, maxValue);
+        return head;
+    }
+
+    public static int minHeight2(Node head) {
+        if (head == null) {
+            return 0;
+        }
+        Node cur = head;
+        Node mostRight = null;
+        int curLevel = 0;
+        int minHeight = Integer.MAX_VALUE;
+        while (cur != null) {
+            mostRight = cur.left;
+            if (mostRight != null) {
+                int leftHeight = 1;
+                while (mostRight.right != null && mostRight.right != cur) {
+                    leftHeight++;
+                    mostRight = mostRight.right;
+                }
+                if (mostRight.right == null) {
+                    mostRight.right = cur;
+                    cur = cur.left;
+                    curLevel += 1;
+                    continue;
+                } else {
+                    if (mostRight.left == null) {
+                        minHeight = Math.min(minHeight, curLevel);
+                    }
+                    curLevel -= leftHeight;
+                    mostRight.right = null;
+                }
+            } else {
+                curLevel++;
+            }
+            cur = cur.right;
+        }
+        //最后遍历对应的最优子树
+        int finalRight = 1;
+        cur = head;
+        while (cur.right != null) {
+            finalRight++;
+            cur = cur.right;
+        }
+        if (cur.left == null && cur.right == null) {
+            minHeight = Math.min(finalRight, minHeight);
+        }
+        return minHeight;
+    }
+
+    public static void main(String[] args) {
+        int treeLevel = 7;
+        int nodeMaxValue = 5;
+        int testTimes = 100000;
+        System.out.println("test begin");
+        for (int i = 0; i < testTimes; i++) {
+            Node head = generateRandomBST(treeLevel, nodeMaxValue);
+            int ans1 = minHeight1(head);
+            int ans2 = minHeight2(head);
+            if (ans1 != ans2) {
+                System.out.println("Oops!");
+            }
+        }
+        System.out.println("test finish!");
+
+    }
+}
---
+++ src/class17/MorrisTraversal.java
@@ -0,0 +1,198 @@
+package class17;
+
+public class MorrisTraversal {
+    public static class Node {
+        public int value;
+        Node left;
+        Node right;
+
+        public Node (int data) {
+            this.value = data;
+        }
+    }
+
+    public static void process(Node head) {
+        if (head == null) {
+            return;
+        }
+        //1
+        process(head.left);
+        //2
+        process(head.right);
+        //3
+    }
+
+    public static void morris(Node head) {
+        if (head == null) {
+            return;
+        }
+        Node cur = head;
+        Node mostRight = null;
+        while (cur != null) {
+            mostRight = cur.left;
+            if (mostRight != null) {
+                while (mostRight.right != null & mostRight.right != cur) {
+                    mostRight = mostRight.right;
+                }
+                if (mostRight.right == null) {
+                    mostRight.right = cur;
+                    cur = cur.left;
+                    continue;
+                } else {
+                    mostRight.right = null;
+                }
+            }
+            cur = cur.right;
+        }
+    }
+
+    public static void morrisPre(Node head) {
+        if (head == null) {
+            return;
+        }
+        Node cur = head;
+        Node mostRight = null;
+        while (cur != null) {
+            mostRight = cur.left;
+            if (mostRight != null) {
+                while (mostRight.right != null & mostRight.right != cur) {
+                    mostRight = mostRight.right;
+                }
+                if (mostRight.right == null) {
+                    mostRight.right = cur;
+                    System.out.println(cur.value + " ");
+                    cur = cur.left;
+                    continue;
+                } else {
+                    mostRight.right = null;
+                }
+            } else {
+                System.out.println(cur.value + " ");
+            }
+            cur = cur.right;
+        }
+        System.out.println();
+    }
+
+    public static void morrisIn(Node head) {
+        if (head == null) {
+            return;
+        }
+        Node cur = head;
+        Node mostRight = null;
+        while (cur != null) {
+            mostRight = cur.left;
+            if (mostRight != null) {
+                while (mostRight.right != null & mostRight.right != cur) {
+                    mostRight = mostRight.right;
+                }
+                if (mostRight.right == null) {
+                    mostRight.right = cur;
+                    cur = cur.left;
+                    continue;
+                } else {
+                    mostRight.right = null;
+                }
+            }
+            //如果说只来一次，肯定要走这一句，如果说第二次，中else分支，也会来到这一句
+            System.out.println(cur.value + " ");
+            cur = cur.right;
+        }
+        System.out.println();
+    }
+
+    public static void morrisPos(Node head) {
+        if (head == null) {
+            return;
+        }
+        Node cur = head;
+        Node mostRight = null;
+        while (cur != null) {
+            mostRight = cur.left;
+            if (mostRight != null) {
+                while (mostRight.right != null & mostRight.right != cur) {
+                    mostRight = mostRight.right;
+                }
+                if (mostRight.right == null) {
+                    mostRight.right = cur;
+                    cur = cur.left;
+                    continue;
+                } else {
+                    mostRight.right = null;
+                    printEdge(cur.left);
+                }
+            }
+            cur = cur.right;
+        }
+        printEdge(head);
+        System.out.println();
+    }
+
+    private static void printEdge(Node head) {
+        Node tail = reverseEdge(head);
+        Node cur = tail;
+        while (cur != null) {
+            System.out.println(cur.value + " ");
+            cur = cur.right;
+        }
+        reverseEdge(head);
+    }
+
+    private static Node reverseEdge(Node head) {
+        Node pre = null;
+        Node next = null;
+        while (head != null) {
+            next = head.right;
+            head.right = pre;
+            pre = head;
+            head = next;
+        }
+        return pre;
+    }
+
+    // for test -- print tree
+    public static void printTree(Node head) {
+        System.out.println("Binary Tree:");
+        printInOrder(head, 0, "H", 17);
+        System.out.println();
+    }
+
+    public static void printInOrder(Node head, int height, String to, int len) {
+        if (head == null) {
+            return;
+        }
+        printInOrder(head.right, height + 1, "v", len);
+        String val = to + head.value + to;
+        int lenM = val.length();
+        int lenL = (len - lenM) / 2;
+        int lenR = len - lenM - lenL;
+        val = getSpace(lenL) + val + getSpace(lenR);
+        System.out.println(getSpace(height * len) + val);
+        printInOrder(head.left, height + 1, "^", len);
+    }
+
+    public static String getSpace(int num) {
+        String space = " ";
+        StringBuffer buf = new StringBuffer("");
+        for (int i = 0; i < num; i++) {
+            buf.append(space);
+        }
+        return buf.toString();
+    }
+
+    public static void main(String[] args) {
+        Node head = new Node(4);
+        head.left = new Node(2);
+        head.right = new Node(6);
+        head.left.left = new Node(1);
+        head.left.right = new Node(3);
+        head.right.left = new Node(5);
+        head.right.right = new Node(7);
+        printTree(head);
+        morrisIn(head);
+        morrisPre(head);
+        morrisPos(head);
+        printTree(head);
+
+    }
+}
